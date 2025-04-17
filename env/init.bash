352a47094901cf7f604fb565b207751af5d6b8f5
John Cavanaugh <59479+cavanaug@users.noreply.github.com>
2025-04-17 01:08:53 -0700

    Cleanup the automount

--- env/init.bash
+++ env/init.bash
@@ -1,12 +1,8 @@
-if [[ -z ${WIN_WINDIR+x} ]]; then
-    if [[ -d /mnt/c/Windows ]]; then
-        WIN_WINDIR='/mnt/c/Windows'
-    else
-        echo "WIN_WINDIR is not configured and I cant find WINDOWS"
-        exit 1
-    fi
+declare -x WIN_WINDIR=${WIN_WINDIR:-/mnt/c/Windows}
+if [[ ! -d $WIN_WINDIR ]]; then
+    echo "ERROR: WIN_WINDIR is not configured and unable to find WINDOWS"
+    exit 1
 fi
-declare -x WIN_WINDIR='/mnt/c/WINDOWS'
 
 if [[ ! ":$PATH:" == *":${WSLUTIL_DIR}/bin:"* ]]; then
     export PATH="${WSLUTIL_DIR}/bin:${PATH}"
@@ -16,10 +12,10 @@ mkdir -p "${HOME}/.cache/wslutil"
 mkdir -p "${HOME}/.local/state/wslutil"
 
 # Mount this WSL distro's root directory under /mnt/wsl
-if [[ ! -d "/mnt/wsl/${WSL_DISTRO_NAME}" ]]; then
+if ! mountpoint -q "/mnt/wsl/${WSL_DISTRO_NAME}"; then
     mkdir -p "/mnt/wsl/${WSL_DISTRO_NAME}"
-    # note the terminating / on the directory name below!
-    ${WIN_WINDIR}/System32/wsl.exe -d "${WSL_DISTRO_NAME}" -u root mount --bind / "/mnt/wsl/${WSL_DISTRO_NAME}/"
+    # HACK:  Clever way to avoid the need to use sudo to run a command
+    wsl.exe -d "${WSL_DISTRO_NAME}" -u root mount --bind / "/mnt/wsl/${WSL_DISTRO_NAME}/"
 fi
 
 # WSL2-specific environment variables and functions
@@ -27,9 +23,9 @@ declare WIN_ENV_FILE="${HOME}/.cache/wslutil/env"
 declare WIN_COMSPEC="/mnt/c/Windows/System32/cmd.exe"
 
 if [[ ! -f "${WIN_COMSPEC}" ]]; then
-    WIN_ERROR="ERROR: Windows system32 cmd.exe not found at ${CMD}"
+    WIN_ERROR="ERROR: Windows system32 cmd.exe not found at ${WIN_COMSPEC}"
     echo "${WIN_ERROR}"
-    return
+    exit 1
 fi
 
 # Set some global environment variables for WINDOWS for use in any cli usage
@@ -52,7 +48,7 @@ while IFS= read -r line; do
     WIN_ENV[${key}]="${value}"
     #WIN_ENV[${key}]="$(printf "%s" "${value}")" || :
     #echo "WIN_ENV[$key]=${WIN_ENV[${key}]}"
-done < ${WIN_ENV_FILE}
+done < "${WIN_ENV_FILE}"
 
 declare -g -x WIN_APPDATA="${WIN_ENV[APPDATA]}"
 declare -g -x WIN_COMPUTERNAME="${WIN_ENV[COMPUTERNAME]}"
@@ -79,10 +75,12 @@ if [[ "${WSL2_GUI_APPS_ENABLED}" ]]; then
     fi
 
     # Fix the scaling for high DPI displays in WSL2 GUI apps
-    if [[ "$(wayland-info | grep refresh | cut -f2 -d' ')" -ge 3840 ]]; then
-        export GDK_SCALE=${GDK_SCALE:-2}
-        export GDK_DPI_SCALE=${GDK_DPI_SCALE:-2}
-        export QT_SCALE_FACTOR=${QT_SCALE_FACTOR:-2}
+    if command -v wayland-info > /dev/null; then
+        if [[ "$(wayland-info | grep refresh | cut -f2 -d' ')" -ge 3840 ]]; then
+            export GDK_SCALE=${GDK_SCALE:-1}
+            export GDK_DPI_SCALE=${GDK_DPI_SCALE:-1}
+            export QT_SCALE_FACTOR=${QT_SCALE_FACTOR:-1}
+        fi
     fi
 else
     # Fallback to X11 if GUI apps are not enabled
